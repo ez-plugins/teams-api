dfed26326137bc386255fb04ad53100f72391a2a
ez-plugins <ezplugins@outlook.com>
2026-04-20 10:57:21 +0200

    fix: updated .gitignore

--- teams-api/target/reports/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"com.skyblockexp.teamsapi.api"},{"l":"com.skyblockexp.teamsapi.event"},{"l":"com.skyblockexp.teamsapi.model"}];updateSearchResults();
